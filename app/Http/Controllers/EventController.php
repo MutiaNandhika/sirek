58dffef857ecf2da01e4381028166e9ca89fe3f6
Rifqifauu <rifqifauzi369@gmail.com>
2024-06-10 09:55:48 +0700

    fix resource

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -12,34 +12,41 @@ class EventController extends Controller
 {
     public function guest(){
         $event = Event::all();
-        return Inertia::render('GuestEvent',['event' => $event]);
+        return Inertia::render('GuestEvent', ['event' => $event]);
     }
+
     public function index(){
         $event = Event::all();
         return Inertia::render('Event', ['event' => $event]);
     }
+
     public function create(){
         $formtype = 'create';
         return Inertia::render('EventForm', ['formtype' => $formtype]);
     }
+
     public function edit($id){
         $event = Event::findOrFail($id);
         $formtype = 'edit';
         return Inertia::render('EventForm', ['event' => $event, 'formtype' => $formtype]);
     }
+
     public function store(Request $request){
-            $validatedData = $request->validate([
+        $validatedData = $request->validate([
             'nama_event' => ['required', 'max:50'],
             'deskripsi' => ['required'],
             'gambar' => ['nullable', 'file', 'max:2048'], 
         ]);
-        if($request->hasFile('gambar')){
-            $filePath = $request->file('gambar')->store('gambar','public');
+
+        if ($request->hasFile('gambar')) {
+            $filePath = $request->file('gambar')->store('gambar', 'public');
             $validatedData['gambar'] = $filePath;
         }
+
         Event::create($validatedData);
         return redirect()->route('event.index')->with('success', 'Event Inserted Successfully');
     }
+
     public function update(Request $request, $id){
         $validatedData = $request->validate([
             'nama_event' => ['required', 'max:50'],
@@ -67,12 +74,13 @@ class EventController extends Controller
         $event->save();
         return redirect()->route('event.index')->with('success', 'Event Successfully Updated');
     }
-   
     
     public function delete($id){
         $event = Event::findOrFail($id);
+        if ($event->gambar) {
+            Storage::disk('public')->delete($event->gambar);
+        }
         $event->delete();
-        return redirect()->route('event.index')->with('success','Event deleted');
+        return redirect()->route('event.index')->with('success', 'Event deleted');
     }
-
 }
